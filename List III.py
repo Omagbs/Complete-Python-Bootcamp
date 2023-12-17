aae3e5e8c9d8e3a99467318bc8a3e0ce45cfd092
Paul Omagbemi <paulwem3000@gmail.com>
2023-12-17 18:15:40 +0100

    create list III file

---
+++ List III.py
@@ -0,0 +1,10 @@
+# This code is to illustrate how list works in python
+# It is a type of container in python that is mutable
+
+# List can be printed from the back
+list1 = [10, 20, 30, 40, 50, 60]
+print(list1[-1])
+
+# an item can be deleted from a list
+del list1[0]
+print(list1)
