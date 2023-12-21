3f746155e4b7822b342f3bc7f877317bd440f913
Paul Omagbemi <paulwem3000@gmail.com>
2023-12-21 22:59:01 +0100

    create Tuple file

---
+++ Tuples.py
@@ -0,0 +1,16 @@
+# Tuples, like list are containers in python
+# Tuples are immutable
+
+# Creating a Tuple
+tuple_1 = ("a", "b", "c")
+print(tuple_1)
+
+# Another way of creating a tuple
+tuple_2 = 1, 2, 3
+print(tuple_2)
+
+# indexng a tuple
+print(tuple_1[2])
+
+#slicing a tuple
+print(tuple_2[1:])
