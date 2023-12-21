ecf73027f6f0fa4e3cffda22d089e06327f1d3fa
Paul Omagbemi <paulwem3000@gmail.com>
2023-12-21 23:16:22 +0100

    create Set file

---
+++ Set.py
@@ -0,0 +1,21 @@
+# Set containers in Python
+# They immutable
+# they make use of the curly brackets {}
+# Duplicates are removed
+
+# Creating a Set
+MySet = {1, 2, 3}
+print(MySet)
+
+# Converting a list to a set also taking out duplicates
+MySet2 = set([1, 2, 3, 2])
+print(MySet2)
+
+# Creating an empty set
+MySet3 = set()
+print(MySet3)
+
+MySet4 = {5, 6, 7}
+MySet5 = {8, 9, 10}
+
+print(6 in MySet4)
