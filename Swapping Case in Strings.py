21cdff20190b6bb40bd953457234a934a9000def
Paul Omagbemi <paulwem3000@gmail.com>
2024-01-29 01:22:01 +0100

    Create Swapping Case in Strings python file

---
+++ Swapping Case in Strings.py
@@ -0,0 +1,5 @@
+# This code shows how to swap cases in strings
+
+# For swapping of the case in a string you the swapcase() function e.g txt.swapcase()
+str_1 = str(input("Enter text:"))
+print("Swapping the case of your text will give:", str_1.swapcase())
