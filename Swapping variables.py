e5961939d2f28896f99530c9f96b4ccb86bd45ee
Paul Omagbemi <paulwem3000@gmail.com>
2024-02-27 21:05:39 +0100

    Create Swapping variables.py

---
+++ Swapping variables.py
@@ -0,0 +1,9 @@
+# This code illustrates swapping variables in python
+
+a = 20
+b = 50
+
+print("Before swapping a = %d and b = %d" % (a, b))
+
+a, b = b, a
+print("After swapping a = %d and b = %d" % (a, b))
