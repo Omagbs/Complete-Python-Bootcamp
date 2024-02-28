8c308091458328aa5eb78e9e28b0a853203128b8
Paul Omagbemi <paulwem3000@gmail.com>
2024-02-28 01:05:28 +0100

    Create Unpacking Tuples.py

---
+++ Unpacking Tuples.py
@@ -0,0 +1,10 @@
+"""
+This code illustrates how tuples can be unpacked
+"""
+
+info = ("John", 27, "Doctor")
+(name, age, profession) = info
+
+print("Name:", name)
+print("Age:", age)
+print("Profession:", profession)
